57d8c7a65ffef07046bdf230b85725bba01c58e8
And <ase@crimea.com>
2023-07-17 22:50:27 +0300

    #7-137 add func_test for multiple users

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -3,8 +3,10 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
-import unittest
+
+MAX_WAIT = 10
 
 
 class NewVisitorsTest(LiveServerTestCase):
@@ -22,13 +24,24 @@ class NewVisitorsTest(LiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        """ wait for row in list table"""
 
-    def test_can_start_a_list_and_retrieve_if_later(self):
-        """ test can start a list and retrieve if later """
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+
+    def test_can_start_a_list_for_one_user(self):
+        """ test: can start a list for one user """
 
         # open home page
         self.browser.get(self.live_server_url)
@@ -50,29 +63,60 @@ class NewVisitorsTest(LiveServerTestCase):
 
         # press <Enter> page refreshed and body contains "1: Buy simple bird"
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.check_for_row_in_list_table('1: Buy simple bird')
+        self.wait_for_row_in_list_table('1: Buy simple bird')
 
         # input "Buy birdcage"
         input_box = self.browser.find_element(By.ID, 'id_new_item')
         input_box.send_keys('Buy birdcage')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # press <Enter> page refreshed and body contains two elements list
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.check_for_row_in_list_table('1: Buy simple bird')
-        self.check_for_row_in_list_table('2: Buy birdcage')
+        self.wait_for_row_in_list_table('1: Buy simple bird')
+        self.wait_for_row_in_list_table('2: Buy birdcage')
 
-        # site contains unique url for link at the birds list
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        """ test_multiple_users_can_start_lists_at_different_urls """
 
-        # press list url and see our birds list on the page
+        # start new list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy simple bird')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy simple bird')
 
-        # close browser
+        # list has unique url
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
-        self.fail('Test final')
+        # new user start
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # get home page, page not present other user data
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy simple bird', page_text)
+        self.assertNotIn('Buy birdcage', page_text)
+
+        # new list create
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # getting new url
+        new_url = self.browser.current_url
+        self.assertRegex(new_url, '/lists/.+')
+        self.assertNotEqual(new_url, edith_list_url)
+
+        # not mark first list
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy simple bird', page_text)
+        self.assertIn('Buy milk', page_text)
+
 
